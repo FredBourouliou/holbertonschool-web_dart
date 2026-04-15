31575d05c23ce5c4e03028041f78319aad9b94b0
Frederic Bourouliou <bourouliou@gmail.com>
2026-04-15 08:18:25 +0200

    Ajout des fichiers pour récupérer et afficher les personnages de Rick et Morty

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> printRmCharacters() async {
+  try {
+    String? url = 'https://rickandmortyapi.com/api/character';
+    while (url != null) {
+      final response = await http.get(Uri.parse(url));
+      final Map<String, dynamic> data = jsonDecode(response.body);
+      final List<dynamic> results = data['results'];
+      for (final character in results) {
+        print(character['name']);
+      }
+      url = data['info']['next'];
+    }
+  } catch (e) {
+    print('error caught: $e');
+  }
+}
